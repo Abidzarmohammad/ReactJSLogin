83bc5ee0954ebc3afbe46423fa4113c6d3e9ae2a
Abidzarmohammad <mohammadabidzar25@gmail.com>
2022-10-16 20:06:48 +0700

    commit all

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+//import component Register
+import Register from './pages/Register';
+
+//import component Login
+import Login from './pages/Login';
+
+//import component Register
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
